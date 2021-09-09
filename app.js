07dd02d437a8a90ec7fb4872a2cd71c8d3414aab
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-09-09 22:40:28 +0100

    Add a Procfile and Set Node Engine

--- app.js
+++ app.js
@@ -62,6 +62,11 @@ app.post("/", function(req,res){
   });
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server is running on port 3000")
 });

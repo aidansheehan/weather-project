e1df78df8f79f30ecaa3325d0dc4781e16e038b1
Aidan Sheehan <aidanmsheehan@gmail.com>
2021-09-09 22:30:50 +0100

    add styling, https error handling, contact page and home links

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 
 const app = express();
 
@@ -13,8 +14,16 @@ app.use(bodyParser.urlencoded( {extended: true} ));
 app.use(express.static("public"));
 
 app.get("/", function(req,res) {
-  res.render("index");
+  res.render("index", {
+    page: "index"
+  });
 });
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    page: "contact"
+  });
+})
 
 app.post("/", function(req,res){
   const apiKey = process.env.API_KEY;
@@ -22,25 +31,36 @@ app.post("/", function(req,res){
   console.log(query);
   const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=metric";
   https.get(url, function(response) {
-    console.log(response.statusCode);
-    response.on("data", function(data) {
-      const weatherData = JSON.parse(data);
-      const location = weatherData.name;
-      const weatherDescription = weatherData.weather[0].description;
-      const localTemp = weatherData.main.temp;
-      const weatherIcon = weatherData.weather[0].icon;
-      const weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-
-      res.render("result", {
-        weatherDescription: weatherDescription,
-        localTemp: localTemp,
-        weatherIconUrl: weatherIconUrl,
-        location: location
+    if (response.statusCode !== 200) {
+      // res.sendStatus(response.statusCode)
+      res.render("error", {
+        page: "error"
       });
-    })
-  })
-});
+    } else {
+      response.on("data", function(data) {
+        const weatherData = JSON.parse(data);
+        console.log(weatherData);
+        const location = weatherData.name + ", " + weatherData.sys.country;
+        const weatherDescription = weatherData.weather[0].description;  /* Lodash this for Capital Letters */
+        const localTemp = weatherData.main.temp;
+        const localTempFeelsLike = weatherData.main.feels_like;
+        const windSpeed = weatherData.wind.speed;
+        const weatherIcon = weatherData.weather[0].icon;
+        const weatherIconUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 
+        res.render("result", {
+          weatherDescription: _.startCase(weatherDescription),
+          localTemp: localTemp,
+          localTempFeelsLike: localTempFeelsLike,
+          windSpeed: Math.round(windSpeed * 3.6),/* meters/second -> km/hour*/
+          weatherIconUrl: weatherIconUrl,
+          location: location,
+          page: "result"
+        });
+      });
+    };
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000")
